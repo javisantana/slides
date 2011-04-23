af4f02aac4711d5ea61ba1ad90f8c21fbbb720ff
javi <javi@agroguia.es>
2011-04-23 19:19:11 +0200

    added django deploy slides

---
+++ django-deploy/wsgi_simple.py
@@ -0,0 +1,10 @@
+
+def application(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+
+    def resp():
+        return "rambo"
+        
+    return resp()
